636e2171ece9a7f247e4ca749871d2b22cfcdb63
Aman Rawat <ar.rawataman@gmail.com>
2024-03-02 15:24:22 +0530

    added templates for tools

--- tools/urls.py
+++ tools/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-    ToolsDetailView
+    ToolsDetailView,
+    HomePageListView,
 )
 
 urlpatterns = [
-    path("<slug:tool_slug>/", ToolsDetailView.as_view(), name="Tool_detail")
+    path("", HomePageListView.as_view(), name="homepage"),
+    path("<slug:tool_slug>/", ToolsDetailView.as_view(), name="Tool_detail"),
 ]
--- tools/views.py
+++ tools/views.py
@@ -1,15 +1,58 @@
 from typing import Any
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Tool
 
 class ToolsDetailView(DetailView):
     model = Tool
-    template_name = "tools/tools_list.html"
     slug_url_kwarg = "tool_slug"
     slug_field = "tool_slug"
-    context_object_name = "tool"
 
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        current_tool = self.kwargs.get("tool_slug")
+        match current_tool:
+            case "username-osint":
+                template_name = "tools/username-osint.html"
+            case "encoding-osint":
+                template_name = "tools/encoding-osint.html"
+            case "decoding-osint":
+                template_name = "tools/decoding-osint.html"
+            case "documentation-osint":
+                template_name = "tools/documentation-osint.html"
+            case "domain-name-osint":
+                template_name = "tools/domain-name-osint.html"
+            case "email-address-osint":
+                template_name = "tools/email-address-osint.html"
+            case "interpol-osint":
+                template_name = "tools/interpol-osint.html"
+            case "ip-address-osint":
+                template_name = "tools/ip-address-osint.html"
+            case "map-osint":
+                template_name = "tools/map-osint.html"
+            case "search-engine-osint":
+                template_name = "tools/search-engine-osint.html"
+            case "telephone-osint":
+                template_name = "tools/telephone-osint.html"
+        self.template_name = template_name
+        return super().get(request, *args, **kwargs)
+
+    
+    
+
+class HomePageListView(ListView):
+    model = Tool
+    template_name = "tools/tools_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().all()
+    
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["all_tools"] = self.get_queryset()
+        return context
+    
